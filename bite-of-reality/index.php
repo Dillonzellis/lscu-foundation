669f4a87b7b02364038d5662577a0c4817a3dc0e
dillonzellis <dillonzellis@gmail.com>
2023-09-19 14:50:44 -0400

    bite initial files

---
+++ bite-of-reality/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>LSCU | Community Development</title>
+  <meta charset="utf-8">
+  <meta name="keywords" content="" />
+  <meta name="author" content="">
+  <meta name="robots" content="" />
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="shortcut icon" href="../images/favicon.ico">
+
+  <!-- Google fonts - witch you want to use - (rest you can just remove) -->
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" media="screen" href="../js/bootstrap/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="../js/megamenu/stylesheets/screen.css" type="text/css" />
+  <link rel="stylesheet" href="../css/default.css" type="text/css" />
+  <link rel="stylesheet" href="../css/layouts.css" type="text/css" />
+  <link rel="stylesheet" href="../css/shortcodes.css" type="text/css" />
+  <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" media="screen" href="../css/responsive-leyouts.css" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="../css/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
+  <link rel="stylesheet" href="../js/tabs/assets/css/responsive-tabs.css" />
+  <link rel="stylesheet" type="text/css" href="../js/tabs/assets/css/responsive-tabs.css">
+  <link rel="stylesheet" href="../css/et-line-font/et-line-font.css">
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-12265204-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-12265204-1');
+  </script>
+
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-QGP42E7TEJ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-QGP42E7TEJ');
+  </script>
+</head>
+
+<body>
+  <div class="site_wrapper">
+    <div class="topbar light topbar-padding">
+      <?php include '../inc/topbar.php'; ?>
+    </div>
+    <div class="clearfix"></div>
+
+    <div id="header" class="header-section style1 pin-style">
+      <?php include '../inc/nav.php'; ?>
+    </div>
+    <div class="clearfix"></div>
+
+
+    <!-- page here -->
+
+
+    <div class="clearfix"></div>
+
+    <section class="section-copyrights sec-moreless-padding">
+      <?php include '../inc/footer.php'; ?>
+    </section>
+    <div class="clearfix"></div>
+    <a href="#" class="scrollup blue"><span style="display:none;">Scroll To Top Button</span></a><!-- end scroll to top of the page-->
+  </div>
+
+  <script type="text/javascript" src="../js/universal/jquery.js"></script>
+  <script src="../js/bootstrap/bootstrap.min.js" type="text/javascript"></script>
+  <script src="../js/megamenu/js/main.js"></script>
+  <script src="../js/mainmenu/jquery.sticky.js"></script>
+  <script src="../js/scrolltotop/totop.js"></script>
+  <script type="text/javascript" src="../js/tabs/smk-accordion.js"></script>
+  <script type="text/javascript" src="../js/tabs/custom.js"></script>
+  <script src="../js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+  <script src="../js/scripts/functions.js" type="text/javascript"></script>
+</body>
+
+</html>
