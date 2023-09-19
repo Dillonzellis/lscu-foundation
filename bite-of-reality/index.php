20bc3921e98c6e7e825104f38dcfaae162b79918
dillonzellis <dillonzellis@gmail.com>
2023-09-19 15:02:21 -0400

    add html

--- bite-of-reality/index.php
+++ bite-of-reality/index.php
@@ -56,7 +56,7 @@
   </script>
 </head>
 
-<body>
+<body class="__variable_395ef7 __variable_1eed36 tw-font-sans tw-text-zinc-800">
   <div class="site_wrapper">
     <div class="topbar light topbar-padding">
       <?php include '../../inc/topbar.php'; ?>
@@ -70,6 +70,136 @@
 
 
     <!-- page here -->
+    <main>
+      <section>
+        <div class="tw-relative"><img alt="hero" loading="lazy" width="2121" height="1413" decoding="async" data-nimg="1" class="tw-max-h-[500px] tw-object-cover tw-brightness-50" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero.ad5f577a.jpg&amp;w=3840&amp;q=75 1x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero.ad5f577a.jpg&amp;w=3840&amp;q=75">
+          <div class="tw-absolute tw-left-1/2 tw-top-1/2 tw--translate-x-1/2 tw--translate-y-1/2 lg:tw-w-[950px]">
+            <div class="tw-flex tw-flex-col tw-items-center tw-rounded-lg tw-shadow-lg md:tw-gap-4 md:tw-bg-white md:tw-p-4 lg:tw-flex-row">
+              <div><img alt="logo" loading="lazy" width="350" height="219" decoding="async" data-nimg="1" class="tw-w-full- tw-drop-shadow-md" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbiteofreality-logo.c75f9017.png&amp;w=384&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbiteofreality-logo.c75f9017.png&amp;w=750&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbiteofreality-logo.c75f9017.png&amp;w=750&amp;q=75"></div>
+              <div class="tw-space-y-2 lg:tw-space-y-4">
+                <div class="tw-hidden tw-font-serif tw-text-2xl tw-font-bold tw-capitalize tw-text-brandingPurple-400 md:tw-block md:tw-text-3xl lg:tw-text-7xl">bite of reality®</div>
+                <div class="tw-hidden tw-font-medium tw-italic md:tw-block lg:tw-text-lg">Credit unions nationwide are working hard to bring financial education to their communities!</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="intro tw-relative tw-py-8">
+        <div class="tw-w-full tw-mx-auto tw-max-w-[1100px] tw-px-10">
+          <div class="tw-grid lg:tw-grid-cols-2">
+            <div class="tw-relative tw-pb-8 lg:tw-pb-[unset]"><img alt="intro image" loading="lazy" width="400" height="450" decoding="async" data-nimg="1" class="tw-relative tw-z-30 tw-mx-auto tw-rounded-md tw-bg-brandingGreen-400 tw-shadow-md tw-shadow-zinc-800/30 lg:tw-m-[unset]" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbite-into-no-bg.8b968f12.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbite-into-no-bg.8b968f12.png&amp;w=828&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbite-into-no-bg.8b968f12.png&amp;w=828&amp;q=75">
+              <div class="tw-absolute tw-left-[23px] tw-top-[23px] tw-z-20 tw-hidden tw-h-[450px] tw-w-[400px] tw-rounded-md tw-border-4 tw-border-brandingGreen-400 tw-drop-shadow-lg lg:tw-block"></div>
+            </div>
+            <div class="tw-space-y-2">
+              <div class="tw-text-2xl tw-font-semibold sm:tw-text-4xl">Fun, Safe, and Interactive</div>
+              <div>
+                <div class="full-bleed-purple tw-bg-brandingPurple-400 tw-py-2">
+                  <div class="tw-pb-2 tw-font-serif tw-text-2xl tw-font-bold tw-text-brandingGreen-400 sm:tw-text-4xl">Bite of Reality</div>
+                  <hr class="tw-pb-2">
+                  <div class="tw-text-white">Bite of Reality is a hands-on app-based simulation that appeals to teens while giving them a taste of real-world financial realities. Teens are given a fictional occupation, salary, credit score, spouse and a child, student loan debt, credit card debt, and medical insurance payments. The teens then walk around to various table-top stations to “purchase” housing, transportation, food, clothing, and other needs. Fortunately, the game also includes a “credit union” to help with their financial needs.</div>
+                </div>
+              </div>
+              <div>This interactive activity will teach teens how to make financial decisions and give them a better understanding of the challenges of living on a budget. Teens will have to deal with a pushy “car salesperson” and a commission-based “realtor,” and weigh their wants versus their needs. There’s even a “Fickle Finger of Fate” that will give the students unexpected “expenses” or “windfalls”—just like in real life.</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="infographic tw-py-8">
+        <div class="tw-w-full tw-mx-auto tw-max-w-[1100px] tw-px-10">
+          <div class="tw-grid tw-gap-8 md:tw-grid-cols-3">
+            <div class="tw-group tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-lg tw-bg-brandingBlue-400 tw-px-4 tw-py-12 tw-shadow-lg tw-shadow-zinc-800/30 tw-transition hover:tw-translate-y-[-10px] hover:tw-bg-brandingGreen-400">
+              <div class="tw-flex tw-h-32 tw-w-32 tw-items-center tw-justify-center tw-rounded-full tw-border-4 tw-border-brandingGreen-400 tw-bg-white group-hover:tw-border-brandingPurple-400"><img alt="" loading="lazy" width="97" height="106" decoding="async" data-nimg="1" class="tw-h-20 tw-w-20 tw-rounded-full tw-object-contain" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsingle-bg.a9efaaeb.png&amp;w=128&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsingle-bg.a9efaaeb.png&amp;w=256&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsingle-bg.a9efaaeb.png&amp;w=256&amp;q=75"></div>
+              <div class="tw-text-center tw-text-white group-hover:tw-text-zinc-800">
+                <div class="tw-pb-4 tw-text-6xl tw-font-semibold tw-underline tw-decoration-brandingGreen-400 group-hover:tw-decoration-brandingPurple-400">23</div>
+                <div class="tw-text-xl tw-capitalize">Events held in July</div>
+              </div>
+            </div>
+            <div class="tw-group tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-lg tw-bg-brandingBlue-400 tw-px-4 tw-py-12 tw-shadow-lg tw-shadow-zinc-800/30 tw-transition hover:tw-translate-y-[-10px] hover:tw-bg-brandingGreen-400">
+              <div class="tw-flex tw-h-32 tw-w-32 tw-items-center tw-justify-center tw-rounded-full tw-border-4 tw-border-brandingGreen-400 tw-bg-white group-hover:tw-border-brandingPurple-400"><img alt="" loading="lazy" width="206" height="146" decoding="async" data-nimg="1" class="tw-h-20 tw-w-20 tw-rounded-full tw-object-contain" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthree-bg.e6a97c08.png&amp;w=256&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthree-bg.e6a97c08.png&amp;w=640&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthree-bg.e6a97c08.png&amp;w=640&amp;q=75"></div>
+              <div class="tw-text-center tw-text-white group-hover:tw-text-zinc-800">
+                <div class="tw-pb-4 tw-text-6xl tw-font-semibold tw-underline tw-decoration-brandingGreen-400 group-hover:tw-decoration-brandingPurple-400">694</div>
+                <div class="tw-text-xl tw-capitalize">students reached in July</div>
+              </div>
+            </div>
+            <div class="tw-group tw-flex tw-flex-col tw-items-center tw-justify-center tw-gap-6 tw-rounded-lg tw-bg-brandingBlue-400 tw-px-4 tw-py-12 tw-shadow-lg tw-shadow-zinc-800/30 tw-transition hover:tw-translate-y-[-10px] hover:tw-bg-brandingGreen-400">
+              <div class="tw-flex tw-h-32 tw-w-32 tw-items-center tw-justify-center tw-rounded-full tw-border-4 tw-border-brandingGreen-400 tw-bg-white group-hover:tw-border-brandingPurple-400"><img alt="" loading="lazy" width="318" height="228" decoding="async" data-nimg="1" class="tw-h-20 tw-w-20 tw-rounded-full tw-object-contain" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmultiple-bg.614f787e.png&amp;w=384&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmultiple-bg.614f787e.png&amp;w=640&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmultiple-bg.614f787e.png&amp;w=640&amp;q=75"></div>
+              <div class="tw-text-center tw-text-white group-hover:tw-text-zinc-800">
+                <div class="tw-pb-4 tw-text-6xl tw-font-semibold tw-underline tw-decoration-brandingGreen-400 group-hover:tw-decoration-brandingPurple-400">36,052</div>
+                <div class="tw-text-xl tw-capitalize">students reached in 2023</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="videos tw-py-8">
+        <div class="tw-bg-brandingPurple-400 tw-py-12">
+          <div class="tw-w-full tw-mx-auto tw-max-w-[1100px] tw-px-10">
+            <div class="">
+              <div class="tw-relative tw-w-full" style="padding-bottom:56.25%"><iframe class="tw-absolute tw-left-0 tw-top-0 tw-h-full tw-w-full tw-rounded-lg tw-shadow tw-shadow-zinc-800/30" src="https://www.youtube.com/embed/gq8gqsymlH0?si=Xpccr5ii5gzVJex4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="benefits tw-py-8">
+        <div class="tw-w-full tw-mx-auto tw-max-w-[1100px] tw-px-10">
+          <div class="tw-relative tw-mb-4 tw-rounded-md tw-bg-brandingBlue-400 tw-py-4 tw-pl-4 tw-text-2xl tw-font-medium tw-text-white tw-shadow-lg tw-shadow-zinc-800/30 before:tw-absolute before:tw-top-[10%] before:tw-z-[-10] before:tw-h-full before:tw-w-full before:tw-border-4 before:tw-border-brandingGreen-400 before:tw-content-[''] md:tw-mb-12 md:tw-w-[25ch] md:tw-px-4 md:tw-py-6 md:tw-pr-16 md:tw-text-4xl">What are the<!-- --> <span class="tw-font-bold tw-underline tw-decoration-brandingGreen-400">Benefits</span> <!-- -->of Bite of Reality?</div>
+          <div class="tw-grid tw-gap-8 md:tw-grid-cols-2">
+            <div>
+              <ul class="tw-list-inside tw-list-disc">
+                <li class="md:tw-text-lg">Teaches basic money management in a safe, fun setting</li>
+                <li class="md:tw-text-lg">Simulates everyday financial challenges and situations</li>
+                <li class="md:tw-text-lg">Introduces the next generation to the benefits of credit unions</li>
+                <li class="md:tw-text-lg">Helps you build relationships with schools, educators, and parents in your communities</li>
+                <li class="md:tw-text-lg">Free to the students</li>
+              </ul>
+            </div>
+            <div class="tw-relative tw-w-full" style="padding-bottom:56.25%"><iframe class="tw-absolute tw-left-0 tw-top-0 tw-h-full tw-w-full tw-rounded-lg tw-shadow-lg tw-shadow-zinc-800/30" src="https://www.youtube.com/embed/ppHQ9k4xAkU?si=hBWWGJumjQebFYq7" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe></div>
+          </div>
+        </div>
+      </section>
+      <section class="img-collage tw-py-8">
+        <div class="tw-flex tw-flex-wrap tw-justify-center tw-bg-brandingBlue-400 tw-py-8 tw-shadow-lg tw-shadow-zinc-800/30"><img alt="" loading="lazy" width="586" height="378" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 md:tw-rounded-tl-lg" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fone.bdf23a04.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fone.bdf23a04.png&amp;w=1200&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fone.bdf23a04.png&amp;w=1200&amp;q=75"><img alt="" loading="lazy" width="594" height="394" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 " style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftwo.e5f1d473.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftwo.e5f1d473.png&amp;w=1200&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftwo.e5f1d473.png&amp;w=1200&amp;q=75"><img alt="" loading="lazy" width="447" height="640" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 md:tw-rounded-tr-lg" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthree.2ad5949d.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthree.2ad5949d.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthree.2ad5949d.png&amp;w=1080&amp;q=75"><img alt="" loading="lazy" width="1237" height="825" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 " style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffour.752738d3.png&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffour.752738d3.png&amp;w=3840&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffour.752738d3.png&amp;w=3840&amp;q=75"><img alt="" loading="lazy" width="800" height="600" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 " style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffive.5846cb88.png&amp;w=828&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffive.5846cb88.png&amp;w=1920&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffive.5846cb88.png&amp;w=1920&amp;q=75"><img alt="" loading="lazy" width="442" height="270" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 " style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsix.05d17b5c.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsix.05d17b5c.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsix.05d17b5c.png&amp;w=1080&amp;q=75"><img alt="" loading="lazy" width="435" height="277" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 md:tw-rounded-bl-lg" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fseven.fa1d8516.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fseven.fa1d8516.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fseven.fa1d8516.png&amp;w=1080&amp;q=75"><img alt="" loading="lazy" width="471" height="273" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 " style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Feight.1f7c394b.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Feight.1f7c394b.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Feight.1f7c394b.png&amp;w=1080&amp;q=75"><img alt="" loading="lazy" width="469" height="278" decoding="async" data-nimg="1" class="tw-w-full tw-max-w-[400px] tw-object-cover tw-shadow-lg tw-shadow-zinc-800/30 tw-brightness-75 md:tw-rounded-br-lg" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnine.3f80b7da.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnine.3f80b7da.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnine.3f80b7da.png&amp;w=1080&amp;q=75"></div>
+      </section>
+      <section class="chapter tw-py-8">
+        <div class="tw-w-full tw-mx-auto tw-max-w-[1100px] tw-px-10">
+          <div class="tw-grid tw-items-center lg:tw-grid-cols-2 lg:tw-gap-4">
+            <div><img alt="" loading="lazy" width="416" height="312" decoding="async" data-nimg="1" class="tw-mx-auto tw-mb-8 tw-rounded-lg tw-object-fill tw-shadow-lg tw-shadow-zinc-800/30 lg:tw-m-[unset]" style="color:transparent" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbite-reality.8d9ca573.jpg&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbite-reality.8d9ca573.jpg&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbite-reality.8d9ca573.jpg&amp;w=1080&amp;q=75"></div>
+            <div>
+              <div class="tw-pb-8">
+                <div class="tw-relative tw-rounded-md tw-py-4 tw-pl-4 tw-font-medium tw-text-white tw-shadow-lg tw-shadow-zinc-800/30 before:tw-absolute before:tw-top-[10%] before:tw-z-[-10] before:tw-h-full before:tw-w-full before:tw-border-4 before:tw-border-brandingGreen-400 before:tw-content-[''] md:tw-mb-12 md:tw-w-[25ch] md:tw-px-4 md:tw-py-6 md:tw-pr-16 md:tw-text-4xl tw-mb-6 tw-w-full tw-bg-brandingPurple-400 tw-text-xl">How can my chapter or credit union<!-- --> <span class="tw-font-bold tw-underline tw-decoration-brandingGreen-400">participate?</span></div>
+                <ul class="tw-list-inside tw-list-disc">
+                  <li class="md:tw-text-lg">Access to the Bite of Reality2 app</li>
+                  <li class="md:tw-text-lg">Training materials for volunteers</li>
+                </ul>
+              </div>
+              <div class="tw-pb-8">
+                <div class="tw-relative tw-rounded-md tw-py-4 tw-pl-4 tw-font-medium tw-text-white tw-shadow-lg tw-shadow-zinc-800/30 before:tw-absolute before:tw-top-[10%] before:tw-z-[-10] before:tw-h-full before:tw-w-full before:tw-border-4 before:tw-border-brandingGreen-400 before:tw-content-[''] md:tw-mb-12 md:tw-w-[25ch] md:tw-px-4 md:tw-py-6 md:tw-pr-16 md:tw-text-4xl tw-mb-6 tw-w-full tw-bg-brandingPurple-400 tw-text-xl">What does my chapter or credit union<!-- --> <span class="tw-font-bold tw-underline tw-decoration-brandingGreen-400">need</span> <!-- -->to put on a Bite of Reality event?</div>
+                <ul class="tw-list-inside tw-list-disc">
+                  <li class="md:tw-text-lg">10 volunteer adults to play the role of various merchants (per 75 students)</li>
+                  <li class="md:tw-text-lg">1 hour of on-site preparation on the day of the event</li>
+                  <li class="md:tw-text-lg">1.5 hours for simulation and debrief</li>
+                  <li class="md:tw-text-lg">50+ teens (target is grades 8–12)</li>
+                  <li class="md:tw-text-lg">A facilitator</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="resources tw-py-8 tw-pb-0">
+        <div class="tw-bg-brandingPurple-400 tw-py-8">
+          <div class="tw-w-full tw-mx-auto tw-max-w-[1100px] tw-px-10 tw-text-white">
+            <div class="tw-text-3xl tw-font-medium tw-underline tw-decoration-brandingGreen-400">Event Resources</div>
+            <ul class="tw-list-inside tw-list-disc tw-pt-4">
+              <li class="tw-text-lg"><a class="tw-capitalize tw-transition hover:tw-text-brandingGreen-400" href="">Facilitator Guide</a></li>
+              <li class="tw-text-lg"><a class="tw-capitalize tw-transition hover:tw-text-brandingGreen-400" href="">Merchant Directions</a></li>
+              <li class="tw-text-lg"><a class="tw-capitalize tw-transition hover:tw-text-brandingGreen-400" href="">request event code</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </main>
+
 
 
     <div class="clearfix"></div>
